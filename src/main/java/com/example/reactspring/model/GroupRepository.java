c633eee5f3312aba6d808b2ce9ef1115f70649f9
Lucero EN <luespinozan@gmail.com>
2019-07-18 10:34:13 -0500

    react y spring boot

---
+++ src/main/java/com/example/reactspring/model/GroupRepository.java
@@ -0,0 +1,11 @@
+package com.example.reactspring.model;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Group findByName(String name);
+    List<Group> findAllByUserId(String id);
+}
